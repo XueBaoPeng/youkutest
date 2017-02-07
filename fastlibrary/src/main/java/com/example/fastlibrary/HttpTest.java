a1d9f2a7548578ccf0831960b7f8afdd5ea607ae
xbp <13519297683@163.com>
2017-02-07 21:47:06 +0800

    添加网络架包

---
+++ fastlibrary/src/main/java/com/example/fastlibrary/HttpTest.java
@@ -0,0 +1,15 @@
+package com.example.fastlibrary;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by Administrator on 2017/2/7 0007.
+ */
+
+public class HttpTest  {
+
+     private OkHttpClient okHttpClient;
+    public static void main(String args[]){
+
+    }
+}
